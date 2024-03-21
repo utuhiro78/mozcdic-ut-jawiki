3550adf4f7ef3c3acbc3acb788b33296733e062d
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-03-21 22:48:56 +0900

    Update generate_jawiki_ut.rb

--- src/generate_jawiki_ut.rb
+++ src/generate_jawiki_ut.rb
@@ -12,29 +12,14 @@ require 'nkf'
 # Wikipediaの記事の例
 # ==============================================================================
 
-# Wikipediaの記事は「タイトル（読み）」を冒頭に書いているものが多い。
+# Wikipediaの記事は「表記（読み）」を冒頭に書いているものが多い。
 # これを使って表記と読みを取得する。
 
-#    <title>生物学</title>
-#    <ns>0</ns>
-#    <id>57</id>
-#    <revision>
-#      <id>81180990</id>
-#      <parentid>79962619</parentid>
-#      <timestamp>2021-01-04T11:40:47Z</timestamp>
-#      <contributor>
-#        <username>MathXplore</username>
-#        <id>1247297</id>
-#      </contributor>
-#      <minor />
-#      <comment>/* 関連項目 */</comment>
-#      <model>wikitext</model>
-#      <format>text/x-wiki</format>
-#      <text bytes="39506" xml:space="preserve">{{複数の問題
-#| 出典の明記 = 2018年11月12日 (月) 08:46 (UTC)
-#| 参照方法 = 2018年11月12日 (月) 08:46 (UTC)
-#}}
-#'''生物学'''（せいぶつがく、{{Lang-en-short|biology}}、
+#       <text bytes="39506" xml:space="preserve">{{複数の問題
+# | 出典の明記 = 2018年11月12日 (月) 08:46 (UTC)
+# | 参照方法 = 2018年11月12日 (月) 08:46 (UTC)
+# }}
+# '''生物学'''（せいぶつがく、{{Lang-en-short|biology}}、
 
 # ==============================================================================
 # generate_jawiki_ut
@@ -81,15 +66,12 @@ def generate_jawiki_ut
 	hyouki.index("プロジェクト:") != nil ||
 	# 表記にスペースがある場合はスキップ
 	# 記事のスペースを削除してから「表記(読み」を検索するので、残してもマッチしない。
-	hyouki.index(" ") != nil ||
-	# 「、」がある場合はスキップ
-	# 記事の「、」で読みを切るので、残してもマッチしない。
-	hyouki.index("、") != nil
+	hyouki.index(" ") != nil
 		return
 	end
 
-	# 読みにならない文字「 」「!?」などを削除したhyouki2を作る
-	hyouki2 = hyouki.tr('\ \.\!\?\-\+\*\=\:\/・。×★☆', '')
+	# 読みにならない文字「!?」などを削除したhyouki2を作る
+	hyouki2 = hyouki.tr('\.\!\?\-\+\*\=\:\/・。×★☆', '')
 
 	# hyouki2が1文字の場合はスキップ
 	if hyouki2[1] == nil
@@ -114,21 +96,33 @@ def generate_jawiki_ut
 	# 記事の量を減らす
 	# ==============================================================================
 
-	lines = $article.gsub("}}'''", "}}\n'''").split("\n")
+	# テンプレート末尾と記事本文の間に改行を入れる
+	lines = $article.gsub("}}'''", "}}\n'''")
+	lines = lines.split("\n")
 
-	# テンプレートを削除
+	s = []
+	p = 0
+
 	lines.length.times do |i|
-		if lines[i][0] == "|" ||
-		lines[i][0] == "*" ||
-		lines[i][0] == "{" ||
-		lines[i][0] == "}"
-			lines[i] = nil
+		# テンプレートを削除
+		if lines[i][0] == "{" ||
+		lines[i][0] == "}" ||
+		lines[i][0] == "|" ||
+		lines[i][0] == "*"
+			next
+		end
+
+		s[p] = lines[i]
+		p = p + 1
+
+		# 記事を最大100行にする
+		if p > 99
+			break
 		end
 	end
 
-	# 記事を最大200行にする
-	# 全部チェックすると時間がかかる。
-	lines = lines.compact[0..199]
+	lines = s
+	s = ""
 
 	# ==============================================================================
 	# 記事から読みを作る
@@ -169,6 +163,10 @@ def generate_jawiki_ut
 		# 読みを「、」で切る
 		# かいとうあいこ、[[1984年]]
 		yomi = yomi.split("、")[0].to_s
+
+		# 読みを「/」で切る
+		# '''卑弥呼'''（ひみこ/ひめこ、
+		yomi = yomi.split("/")[0].to_s
 
 		# 読みが2文字以下の場合はスキップ
 		if yomi[2] == nil
