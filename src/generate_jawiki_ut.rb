daab02bed062e4a3723a07d875194d436324df2a
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-06-20 23:30:43 +0900

    Simplified

--- src/generate_jawiki_ut.rb
+++ src/generate_jawiki_ut.rb
@@ -225,11 +225,7 @@ url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_
 $id_mozc = URI.open(url).read.split(" 名詞,一般,")[0]
 $id_mozc = $id_mozc.split("\n")[-1]
 
-url = "https://dumps.wikimedia.org/jawiki/"
-jawiki_date = URI.open(url).read.split("<a href=\"")[-2]
-jawiki_date = jawiki_date.split("/")[0]
-
-`wget -N https://dumps.wikimedia.org/jawiki/#{jawiki_date}/jawiki-#{jawiki_date}-pages-articles-multistream.xml.bz2`
+`wget -N https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles-multistream.xml.bz2`
 
 # Parallel のプロセス数を「物理コア数 - 1」にする
 core_num = `grep cpu.cores /proc/cpuinfo`.chomp.split(": ")[-1].to_i - 1
@@ -237,7 +233,7 @@ core_num = `grep cpu.cores /proc/cpuinfo`.chomp.split(": ")[-1].to_i - 1
 $dicfile = File.new(dicname, "w")
 jawiki_fragment = ""
 
-reader = Bzip2::FFI::Reader.open('jawiki-' + jawiki_date + '-pages-articles-multistream.xml.bz2')
+reader = Bzip2::FFI::Reader.open('jawiki-latest-pages-articles-multistream.xml.bz2')
 
 puts "Reading..."
 
