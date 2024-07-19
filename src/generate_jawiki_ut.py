263fe3243f98e1a9438f8f4c3bf1ae5ade371a6c
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-19 22:25:41 +0900

    Fix style

--- src/generate_jawiki_ut.py
+++ src/generate_jawiki_ut.py
@@ -10,28 +10,21 @@ import html
 import jaconv
 import re
 import subprocess
-import unicodedata
 import urllib.request
 from multiprocessing import Pool, cpu_count
-
-# ==============================================================================
-# Wikipediaの記事の例
-# ==============================================================================
-
-# Wikipediaの記事は「表記（読み）」を冒頭に書いているものが多い。
-# これを使って表記と読みを取得する。
-
-#  xml:space="preserve">
-# <title>あいの里公園駅</title>
-# '''あいの里公園駅'''（あいのさとこうえんえき）は、
-
-# ==============================================================================
-# generate_jawiki_ut
-# ==============================================================================
+from unicodedata import normalize
 
 def generate_jawiki_ut(article):
-	global dicfile
-	global id_mozc
+	# ==============================================================================
+	# Wikipediaの記事の例
+	# ==============================================================================
+
+	# Wikipediaの記事は「タイトル（読み）」を冒頭に書いているものが多い。
+	# これを使って表記と読みを取得する。
+
+	#  xml:space="preserve">
+	# <title>あいの里公園駅</title>
+	# '''あいの里公園駅'''（あいのさとこうえんえき）は、
 
 	# ==============================================================================
 	# タイトルから表記を作る
@@ -50,7 +43,7 @@ def generate_jawiki_ut(article):
 	article = article[1]
 
 	# 全角英数を半角に変換
-	hyouki = unicodedata.normalize('NFKC', title)
+	hyouki = normalize("NFKC", title)
 
 	# 表記を「 (」で切る
 	# 田中瞳 (アナウンサー)
@@ -59,7 +52,7 @@ def generate_jawiki_ut(article):
 	# 表記が26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
 	# 内部用のページをスキップ
 	# 表記にスペースがある場合はスキップ
-	# （記事のスペースを削除してから「表記(読み」を検索するので、残してもマッチしない）
+	#     記事のスペースを削除してから「表記(読み」を検索するので、残してもマッチしない。
 	if len(hyouki) > 25 or \
 	"(曖昧さ回避)" in hyouki or \
 	"Wikipedia:" in hyouki or \
@@ -72,17 +65,17 @@ def generate_jawiki_ut(article):
 	" " in hyouki:
 		return
 
-	# 読みにならない文字「!?」などを削除したhyouki2を作る
-	hyouki2 = hyouki.translate(str.maketrans('', '', '.!?-+*=:/・。×★☆'))
+	# 読みにならない文字「!?」などを削除したhyouki_stripを作る
+	hyouki_strip = hyouki.translate(str.maketrans('', '', '.!?-+*=:/・。×★☆'))
 
-	# hyouki2が1文字の場合はスキップ
-	if len(hyouki2) < 2:
+	# hyouki_stripが1文字の場合はスキップ
+	if len(hyouki_strip) < 2:
 		return
 
-	# hyouki2がひらがなとカタカナだけの場合は、読みをhyouki2から作る
+	# hyouki_stripがひらがなとカタカナだけの場合は、読みをhyouki_stripから作る
 	# さいたまスーパーアリーナ
-	if hyouki2 == ''.join(re.findall('[ぁ-ゔァ-ヴー]', hyouki2)):
-		yomi = jaconv.kata2hira(hyouki2)
+	if hyouki_strip == ''.join(re.findall('[ぁ-ゔァ-ヴー]', hyouki_strip)):
+		yomi = jaconv.kata2hira(hyouki_strip)
 		yomi = yomi.translate(str.maketrans('ゐゑ', 'いえ'))
 
 		entry = [yomi, id_mozc, id_mozc, "8000", hyouki]
@@ -132,7 +125,7 @@ def generate_jawiki_ut(article):
 		entry = lines[i]
 
 		# 全角英数を半角に変換
-		entry = unicodedata.normalize('NFKC', entry)
+		entry = normalize("NFKC", entry)
 
 		# HTML特殊文字を変換
 		entry = html.unescape(entry)
@@ -185,7 +178,7 @@ def generate_jawiki_ut(article):
 
 		# 読みが「ー」で始まる場合はスキップ
 		# 読みが全てカタカナの場合はスキップ
-		# （ミュージシャン一覧(グループ)）
+		#     ミュージシャン一覧(グループ)
 		if yomi[0] == "ー" or \
 		yomi == ''.join(re.findall('[ァ-ヴー]', yomi)):
 			continue
@@ -206,53 +199,57 @@ def generate_jawiki_ut(article):
 			fcntl.flock(dicfile, fcntl.LOCK_UN)
 		return
 
-# ==============================================================================
-# main
-# ==============================================================================
+def main():
+	global dicname
+	global id_mozc
 
-dicname = "mozcdic-ut-jawiki.txt"
+	dicname = "mozcdic-ut-jawiki.txt"
 
-# Mozc の一般名詞のID
-url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_oss/id.def"
-with urllib.request.urlopen(url) as response:
-	id_mozc = response.read().decode()
+	# Mozc の一般名詞のID
+	url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_oss/id.def"
+	with urllib.request.urlopen(url) as response:
+		id_mozc = response.read().decode()
 
-id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
+	id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
 
-subprocess.run(["wget", "-N", "https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles-multistream.xml.bz2"])
+	subprocess.run(["wget", "-N", "https://dumps.wikimedia.org/jawiki/latest/jawiki-latest-pages-articles-multistream.xml.bz2"])
 
-with open(dicname, "w", encoding="utf-8") as dicfile:
-	dicfile.write("")
+	with open(dicname, "w", encoding="utf-8") as dicfile:
+		dicfile.write("")
 
-jawiki_fragment = ""
-cache_size = 200 * 1024 * 1024
-core_num = cpu_count()
+	article_fragment = ""
+	cache_size = 200 * 1024 * 1024
+	core_num = cpu_count()
 
-with bz2.open("jawiki-latest-pages-articles-multistream.xml.bz2", "rt", encoding="utf-8") as reader:
-	while True:
-		jawiki = reader.read(cache_size)
+	with bz2.open("jawiki-latest-pages-articles-multistream.xml.bz2", "rt", encoding="utf-8") as reader:
+		while True:
+			articles = reader.read(cache_size)
 
-		if jawiki == "":
-			break
+			# 最後まで読み切ったら終了
+			if articles == "":
+				break
 
-		jawiki = jawiki.split("  </page>")
-		jawiki[0] = jawiki_fragment + jawiki[0]
+			articles = articles.split("  </page>")
+			articles[0] = article_fragment + articles[0]
 
-		# 記事の断片を別名で保存
-		jawiki_fragment = jawiki[-1]
+			# 記事の断片を別名で保存
+			article_fragment = articles[-1]
 
-		# 記事の断片を削除
-		jawiki = jawiki[0:-1]
+			# 記事の断片を削除
+			articles = articles[0:-1]
 
-		with Pool(processes=core_num) as pool:
-			pool.map(generate_jawiki_ut, jawiki)
-		pool.join()
+			with Pool(processes=core_num) as pool:
+				pool.map(generate_jawiki_ut, articles)
+			pool.join()
 
-with open(dicname, "r", encoding="utf-8") as file:
-	lines = file.readlines()
+	with open(dicname, "r", encoding="utf-8") as file:
+		lines = file.readlines()
 
-# 重複する行を削除
-lines = sorted(list(set(lines)))
+	# 重複する行を削除
+	lines = sorted(list(set(lines)))
 
-with open(dicname, "w", encoding="utf-8") as file:
-	file.writelines(lines)
+	with open(dicname, "w", encoding="utf-8") as file:
+		file.writelines(lines)
+
+if __name__ == "__main__":
+	main()
