29dd6d3202119d88a2356a11300b7b338f5cb950
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-29 01:27:13 +0900

    Code style fixes

--- src/generate_jawiki_ut.py
+++ src/generate_jawiki_ut.py
@@ -50,11 +50,12 @@ def generate_jawiki_ut(article):
     # 田中瞳 (アナウンサー)
     hyouki = hyouki.split(' (')[0]
 
-    # 表記が26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
-    # 内部用のページをスキップ
     # 表記にスペースがある場合はスキップ
     #     記事のスペースを削除してから「表記(読み」を検索するので、残してもマッチしない。
-    if len(hyouki) > 25 or \
+    # 表記が26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
+    # 内部用のページをスキップ
+    if " " in hyouki or \
+            len(hyouki) > 25 or \
             "(曖昧さ回避)" in hyouki or \
             "Wikipedia:" in hyouki or \
             "ファイル:" in hyouki or \
@@ -62,8 +63,7 @@ def generate_jawiki_ut(article):
             "Help:" in hyouki or \
             "Template:" in hyouki or \
             "Category:" in hyouki or \
-            "プロジェクト:" in hyouki or \
-            " " in hyouki:
+            "プロジェクト:" in hyouki:
         return
 
     # 読みにならない文字「!?」などを削除したhyouki_stripを作る
@@ -74,7 +74,7 @@ def generate_jawiki_ut(article):
         return
 
     # hyouki_stripがひらがなとカタカナだけの場合は、読みをhyouki_stripから作る
-    # さいたまスーパーアリーナ
+    #     さいたまスーパーアリーナ
     if hyouki_strip == ''.join(re.findall('[ぁ-ゔァ-ヴー]', hyouki_strip)):
         yomi = jaconv.kata2hira(hyouki_strip)
         yomi = yomi.translate(str.maketrans('ゐゑ', 'いえ'))
@@ -97,17 +97,17 @@ def generate_jawiki_ut(article):
 
     entry = []
 
-    for i in range(len(lines)):
+    for line in lines:
         # テンプレートを削除
         # 収録語は「'''盛夏'''（せいか）」が最小なので、12文字以下の行はスキップ
-        if len(lines[i]) < 13 or \
-                lines[i][0] == "{" or \
-                lines[i][0] == "}" or \
-                lines[i][0] == "|" or \
-                lines[i][0] == "*":
+        if len(line) < 13 or \
+                line[0] == "{" or \
+                line[0] == "}" or \
+                line[0] == "|" or \
+                line[0] == "*":
             continue
 
-        entry.append(lines[i])
+        entry.append(line)
 
         # 記事が100行になったらbreak
         if len(entry) == 100:
@@ -120,39 +120,37 @@ def generate_jawiki_ut(article):
     # 記事から読みを作る
     # ==============================================================================
 
-    for i in range(len(lines)):
-        entry = lines[i]
-
+    for line in lines:
         # 全角英数を半角に変換
-        entry = normalize("NFKC", entry)
+        line = normalize("NFKC", line)
 
         # HTML特殊文字を変換
-        entry = html.unescape(entry)
+        line = html.unescape(line)
 
         # 「{{」から「}}」までを削除
         # '''皆藤 愛子'''{{efn2|一部のプロフィールが「皆'''籐'''（たけかんむり）」と
         # なっているが、「皆'''藤'''（くさかんむり）」が正しい。}}（かいとう あいこ、
         # [[1984年]][[1月25日]] - ）は、
-        if "{{" in entry:
-            entry = re.sub(r'{{.*?}}', '', entry)
+        if "{{" in line:
+            line = re.sub(r'{{.*?}}', '', line)
 
         # 「<ref」から「</ref>」までを削除
         # '''井上 陽水'''（いのうえ ようすい<ref name="FMPJ">{{Cite web|和書|title=
         # アーティスト・アーカイヴ 井上陽水 {{small|イノウエヨウスイ}}|url=
         # https://www.kiokunokiroku.jp/artistarchives|work=記憶の記録 LIBRARY|
         # publisher=[[日本音楽制作者連盟]]|accessdate=2023-06-21}}</ref>、[[1948年]]
-        entry = re.sub(r'<ref.*?<\/ref>', '', entry)
+        line = re.sub(r'<ref.*?<\/ref>', '', line)
 
-        # 「<ref name="example" />」を削除
-        entry = re.sub(r'<ref\ name.*?\/>', '', entry)
+        # 「<ref name="雑誌1" />」を削除
+        line = re.sub(r'<ref\ name.*?\/>', '', line)
 
         # スペースと「'"「」『』」を削除
         # '''皆藤 愛子'''(かいとう あいこ、[[1984年]]
-        entry = entry.translate(str.maketrans('', '', ' "\'「」『』'))
+        line = line.translate(str.maketrans('', '', ' "\'「」『』'))
 
         # 「表記(読み」から読みを取得
-        if hyouki + "(" in entry:
-            yomi = entry.split(hyouki + "(")[1]
+        if hyouki + "(" in line:
+            yomi = line.split(hyouki + "(")[1]
         else:
             continue
 
