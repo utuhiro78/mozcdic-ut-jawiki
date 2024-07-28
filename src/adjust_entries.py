11f35fe1d504d8ca62902c8a84c38b729a142cd5
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-28 23:46:23 +0900

    Small fixes

--- src/adjust_entries.py
+++ src/adjust_entries.py
@@ -44,36 +44,35 @@ for i in range(len(lines)):
     if hyouki.endswith(" "):
         hyouki = hyouki[:-1]
 
-    # 読みにならない文字「 !?」などを削除したhyouki2を作る
-    hyouki2 = hyouki.translate(str.maketrans('', '', ' .!?-+*=:/・。×★☆'))
+    # 読みにならない文字「 !?」などを削除したhyouki_stripを作る
+    hyouki_strip = hyouki.translate(str.maketrans('', '', ' .!?-+*=:/・。×★☆'))
 
-    # hyouki2がひらがなとカタカナだけの場合は、読みをhyouki2から作る
-    if hyouki2 == ''.join(re.findall('[ぁ-ゔァ-ヴー]', hyouki2)):
-        yomi = jaconv.kata2hira(hyouki2)
+    # hyouki_stripがひらがなとカタカナだけの場合は、読みをhyouki_stripから作る
+    if hyouki_strip == ''.join(re.findall('[ぁ-ゔァ-ヴー]', hyouki_strip)):
+        yomi = jaconv.kata2hira(hyouki_strip)
         yomi = yomi.translate(str.maketrans('ゐゑ', 'いえ'))
 
     # 読みが2文字以下の場合はスキップ
-    # hyouki2が1文字の場合はスキップ
-    # hyoukiが26文字以上の場合はスキップ
-    # 読みの文字数がhyouki2の4倍を超える場合はスキップ
-    # （さくらざかふぉーてぃーしっくす[15文字] 櫻坂46[4文字]」までは残す。）
-    # hyouki2の文字数*3が読みのバイト数より小さい場合はスキップ
-    # （「あいてぃー[5文字] ITエンジニア[17bytes]」をスキップ。）
-    # （「すのーまん[5文字] SNOWMAN[7bytes]」は残す。）
+    # hyouki_stripが1文字の場合はスキップ
+    # hyoukiが26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
+    # 読みの文字数をhyouki_stripの文字数で割ったときに、4を超える場合はスキップ
+    #     「さくらざかふぉーてぃーしっくす[15文字] 櫻坂46[4文字]」までは残す。
+    # hyouki_stripのバイト数を読みの文字数で割ったときに、3を超える場合はスキップ
+    #     「あいてぃー[5文字] ITエンジニア[17bytes]」をスキップ。
     # 読みがひらがな以外を含む場合はスキップ
     # hyoukiがコードポイントを含む場合はスキップ
     if len(yomi) < 3 or \
-            len(hyouki2) < 2 or \
+            len(hyouki_strip) < 2 or \
             len(hyouki) > 25 or \
-            len(yomi) > len(hyouki2) * 4 or \
-            len(yomi) * 3 < len(hyouki2.encode()) or \
+            len(yomi) / len(hyouki_strip) > 4 or \
+            len(hyouki_strip.encode()) / len(yomi) > 3 or \
             yomi != ''.join(re.findall('[ぁ-ゔー]', yomi)) or \
             '\\u' in hyouki:
         continue
 
-    # hyouki2の数字をつなげると101以上になる場合はスキップ
-    # （「国道120号」「3月26日」はスキップ。「100円ショップ」は残す。）
-    n = re.sub(r"\D", "", hyouki2)
+    # hyouki_stripの数字をつなげると101以上になる場合はスキップ
+    #     「国道120号」「3月26日」はスキップ。「100円ショップ」は残す。
+    n = re.sub(r"\D", "", hyouki_strip)
 
     if n != "" and int(n) > 100:
         continue
